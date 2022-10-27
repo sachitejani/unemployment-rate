a84c609fa0d77cbdcd8a46b4fe04e1e6d471255b
sachitejani <skt39@georgetown.edu>
2022-10-26 23:00:50 -0400

    Final update to part A

--- app/unemployment.py
+++ app/unemployment.py
@@ -4,8 +4,9 @@
 import os
 import json
 from pprint import pprint
-
 import requests
+from statistics import mean
+
 
 API_KEY = os.getenv("API_KEY")
 
@@ -14,14 +15,19 @@ request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={
 response = requests.get(request_url)
 
 parsed_response = json.loads(response.text)
-print(type(parsed_response))
-pprint(parsed_response)
+# print(type(parsed_response))
+# pprint(parsed_response)
 
 #breakpoint()
 
+
+# Part A
 latest = parsed_response["data"][0]
+latest_value = latest["value"]
 latest_date = latest["date"]
 
 print("-------------------------")
 print("LATEST UNEMPLOYMENT RATE:")
-print(f"{latest}%", "as of", latest_date)
+print(f"{latest_value}%", "as of", latest_date)
+
+
